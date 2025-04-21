66326e9215910fdf6c099084ce749eaa440f3164
Brian E McGreal <bmcgreal@uw.edu>
2025-04-21 12:10:42 -0700

    goober

--- Lab-1/Team-1/Lab1-ARIMA-2025-McGrealSANDBOX.R
+++ Lab-1/Team-1/Lab1-ARIMA-2025-McGrealSANDBOX.R
@@ -1,8 +1,10 @@
 # load packages
 library(atsalibrary)
+# library(ggfortify)
 library(ggplot2)
 library(patchwork)
 library(tidyverse)
+library(zoo)
 
 ## STUFF FROM .rmd
 # create date column (first of each month)
@@ -284,3 +286,56 @@ points(WAvalue.test)
 plot(WAvalue.fc$mean)
 points(WAvalue.test)
 
+## NEW SNADBOX (pt. 2)
+# plotting this bs with ggplot
+class(WAvalue.fc$mean)
+df <- data.frame(date=as.Date(as.yearmon(time(WAvalue.fc$mean))), Y=as.matrix(WAvalue.fc$mean))
+test <- data.frame(date=as.Date(as.yearmon(time(WAvalue.test))), Y=as.matrix(WAvalue.test))
+ggplot(data=df, aes(x=date, y=Y)) + 
+  geom_line() +
+  geom_point(data=test, aes(x=date, y=Y)) +
+  theme_classic()
+
+# making interacted forecasts with CI
+time = as.Date(as.yearmon(time(WAlands.fc$mean)))
+landed <- c(exp(WAlands.fc$mean))
+landed.lb80 <- c(WAlands.fc$lower[,1])
+landed.lb95 <- c(WAlands.fc$lower[,2])
+landed.ub80 <- c(WAlands.fc$upper[,1])
+landed.ub95 <- c(WAlands.fc$upper[,2])
+prices <- c(WAprice.fc$mean)
+prices.lb80 <- c(WAprice.fc$lower[,1])
+prices.lb95 <- c(WAprice.fc$lower[,2])
+prices.ub80 <- c(WAprice.fc$upper[,1])
+prices.ub95 <- c(WAprice.fc$upper[,2])
+
+df <- data.frame(time, 
+                 landed, landed.lb80, landed.lb95, landed.ub80, landed.ub95,
+                 prices, prices.lb80, prices.lb95, prices.ub80, prices.ub95)
+
+df$landedCI80 <- df$landed.ub80 - df$landed.lb80
+df$landedCI95 <- df$landed.ub95 - df$landed.lb95
+df$pricesCI80 <- df$prices.ub80 - df$prices.lb80
+df$pricesCI95 <- df$prices.ub95 - df$prices.lb95
+
+df$landedSE80 <- df$landedCI80/2/1.282
+df$landedSE95 <- df$landedCI95/2/1.96
+df$pricesSE80 <- df$pricesCI80/2/1.282
+df$pricesSE95 <- df$pricesCI95/2/1.96
+
+df$compound <- df$prices*exp(df$landed)
+df$Vcompound <- (exp(df$landed)^2*df$pricesSE95^2) + 
+                (df$prices^2*df$landedSE95^2) +
+                (2*df$prices*df$landed*cov(df$prices, df$landed)) +
+                (df$landedSE95^2*df$pricesSE95^2) +
+                cov(df$prices, df$landed)^2
+df$SEcompound <- sqrt(df$Vcompound)
+df$compound.lb95 <- df$compound - 1.96*df$SEcompound
+df$compound.ub95 <- df$compound + 1.96*df$SEcompound
+df$compound.lb80 <- df$compound - 1.282*df$SEcompound
+df$compound.ub80 <- df$compound + 1.282*df$SEcompound
+
+ggplot(data = df, aes(x=time, y=compound)) +
+  geom_ribbon(aes(ymin = compound.lb95, ymax = compound.ub95), fill = "skyblue1", alpha = 0.5) +
+  geom_line() +
+  theme_classic() 

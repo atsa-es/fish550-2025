aa68e9ff9ac0e338ae8698ee121d9b6b166aeaf1
themacfreezie <62110677+themacfreezie@users.noreply.github.com>
2025-04-28 17:07:16 -0700

    wokring on lab 3

--- Lab-3/Team-2/mcgreal_sandbox.R
+++ Lab-3/Team-2/mcgreal_sandbox.R
@@ -1,7 +1,16 @@
 ## load MARSS
 library(MARSS)
+library(reshape2)
+library(tidyverse)
 
 ## load the data (there are 3 datasets contained here)
 data(lakeWAplankton, package = "MARSS")
-all_dat <- lakeWAplanktonTrans
+all_dat <- as.data.frame(lakeWAplanktonTrans)
 
+all_dat$Date <- as.yearmon(paste(all_dat$Year, all_dat$Month), "%Y %m")
+
+for ( i in seq(3,length( all_dat )-1,1) ) plot(all_dat[,i], x=all_dat$Date, ylab=names(all_dat[i]),type="l")
+
+# dropping years pre-1980 and critters with poor data
+data <- subset(all_dat, Year  >= 1980)
+data <- data[-c(1, 2, 9, 18)]
